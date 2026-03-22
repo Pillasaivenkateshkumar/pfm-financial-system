feeca71567b3ac6997bbcf119eb3edd0a4b701ee
EC2 Default User <ec2-user@ip-172-31-20-194.ec2.internal>
2026-03-22 03:12:40 +0000

    Update settings for production

--- pfm/settings.py
+++ pfm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3n8=5o9-uioqj19%j+6(9is*tr2j*y5qs-mn7&bx)b!f7uze3)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -115,7 +115,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+
+STATIC_URL = '/static/'
+
+# Folder where YOU keep static files (optional but recommended)
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+# Folder where Django collects static files for deployment
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
